f0bf41dfcab80c57a2a0b465ce28c2dc0a02ba34
Franck Kengne <franck.kengne@cse-corp.com>
2025-06-02 18:36:07 -0400

    updating from init

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
-from unittest.mock import MagicMock
+from unittest.mock import patch, MagicMock
 
-@pytest.fixture
-def mock_connection():
-    return MagicMock()
+@pytest.fixture(scope="module")
+def test_config():
+    return {
+        "pat": "dummy_pat",
+        "organization": "test_org",
+        "storage_connection_string": "test_conn_str",
+        "container_name": "test_container"
+    }
